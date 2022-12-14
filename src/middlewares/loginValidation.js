49fae19c4c9366f3e3a1e9edcbf934afd73ba8e6
Lucas Victor <lucassvtf@gmail.com>
2022-12-14 18:55:12 -0300

    Requisito 03 e 04- arrumando erros de lint

--- src/middlewares/loginValidation.js
+++ src/middlewares/loginValidation.js
@@ -3,26 +3,17 @@ const loginValidation = (req, res, next) => {
     const regexEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
 
     if (!email) {
-        return res.status(400).json({
-            message: 'O campo "email" é obrigatório',
-          });
+        return res.status(400).json({ message: 'O campo "email" é obrigatório' });
     }
     if (!password) {
-        return res.status(400).json({
-            message: 'O campo "password" é obrigatório',
-          });
+        return res.status(400).json({ message: 'O campo "password" é obrigatório' });
     }
     if (!regexEmail.test(email)) {
-        return res.status(400).json({
-            message: 'O "email" deve ter o formato "email@email.com"',
-          });
+        return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
     }
     if (password.length < 6) {
-        return res.status(400).json({
-            message: 'O "password" deve ter pelo menos 6 caracteres',
-          });
+        return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
     }
-
     next();
 };
 
